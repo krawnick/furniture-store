b21b404fe42eb62e0f7a32662ff0d9009af2d960
Nikita Kravtsov <kraw.nick@gmail.com>
2023-04-23 17:31:37 +0500

    added cart to Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,11 @@
+import { FaShoppingCart } from 'react-icons/fa'
+import { useState } from 'react'
+import classNames from 'classnames'
 import style from './Header.module.css'
 
 export function Header() {
+  let [cartOpen, setCartOpen] = useState(false)
+
   return (
     <header>
       <div>
@@ -10,7 +15,15 @@ export function Header() {
           <li>Контакты</li>
           <li>Кабинет</li>
         </ul>
+        <FaShoppingCart
+          className={classNames(
+            style.shop_cart_button,
+            cartOpen ? style.active : ''
+          )}
+          onClick={() => setCartOpen((cartOpen = !cartOpen))}
+        />
       </div>
+      {cartOpen && <div className={style.cart}></div>}
       <div className={style.presentations}></div>
     </header>
   )
