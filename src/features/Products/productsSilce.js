b30b4681e825bfc2f39b5a0b996d3cd63441340a
krawnick <kraw.nick@gmail.com>
2023-07-12 19:57:58 +0500

    products: add loading

--- src/features/Products/productsSilce.js
+++ src/features/Products/productsSilce.js
@@ -1,10 +1,36 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+
+export const loadProducts = createAsyncThunk(
+  '@@products/load-products',
+  (_, { extra: { client, api } }) => {
+    return client.get(api.BASE_URL)
+  }
+)
+
+const initialState = {
+  status: 'idle',
+  error: null,
+  list: [],
+}
 
 const productsSLice = createSlice({
   name: '@@products',
-  initialState: [],
-  reducers: {
-    addLoad: () => {},
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(loadProducts.pending, (state) => {
+        state.status = 'loading'
+        state.error = null
+      })
+      .addCase(loadProducts.rejected, (state, action) => {
+        state.status = 'rejected'
+        state.error = action.payload || action.meta.error
+      })
+      .addCase(loadProducts.fulfilled, (state, action) => {
+        state.status = 'received'
+        state.list = action.payload.data
+      })
   },
-  extraReducers: {},
 })
+
+export const productsReducer = productsSLice.reducer
--- src/store.js
+++ src/store.js
@@ -1,7 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { categoriesReducer } from './features/Categories/catregoriesSlice'
+import { productsReducer } from './features/Products/productsSilce'
+import axios from 'axios'
+import * as api from './config'
 
 export const store = configureStore({
-  reducer: { categories: categoriesReducer },
+  reducer: { categories: categoriesReducer, products: productsReducer },
   devTools: true,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgumets: {
+          client: axios,
+          api,
+        },
+      },
+      serializableCheck: false,
+    }),
 })
